33a1a1b2ac349d16821a45f848dbc36322964074
Johanna Larsson Cornejo <jc222fi@student.lnu.se>
2015-04-24 00:41:21 +0200

    Bunch of enemies

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    
+    public GameObject enemyPrefab;
+    public Transform enemySpawn;
+    public Text scoreText;
+    public Vector2 spawnValues;
+    public int enemyCount;
+
+    private int score;
+    // Use this for initialization
+    void Start()
+    {
+        EnemySpawn();
+        score = 0;
+        UpdateScore();
+    }
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+    void EnemySpawn()
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            Vector2 spawnPosition = new Vector2(spawnValues.x, Random.Range(-spawnValues.y, spawnValues.y));
+            Instantiate(enemyPrefab, spawnPosition, enemySpawn.rotation); 
+        }
+    }
+}
